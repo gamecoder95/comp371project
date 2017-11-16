95f32980e49f27a9d5d8a414372e63d269d09f54
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2017-11-15 19:27:31 -0500

    Tested object generation
    Did so just by measuring time, works!

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -4,11 +4,15 @@
 #include <string>
 #include "ObjectContainer.h"
 #include "Cube.h"
+#include <cstdlib> // for rand() and srand() -> testing object generation
+#include <ctime> // for time() -> testing object generation
 using namespace std;
 
 const GLuint WIDTH = 800;
 const GLuint HEIGHT = 800;
 
+// The projection and view matrices here are just testing things, will remove when we have 
+// the official camera class
 glm::mat4 projection_matrix;
 const glm::vec3 center(0.0f, 0.0f, 0.0f);
 const glm::vec3 up(0.0f, 1.0f, 0.0f);
@@ -59,11 +63,16 @@ int main()
 	mainShader.useProgram();
 
 	ObjectContainer obj_container;
-	obj_container.addObject(new Cube(&mainShader, glm::vec3(0.5f, 0.0f, 0.0f)));
-	obj_container.addObject(new Cube(&mainShader, glm::vec3(-0.5f, 0.0f, 0.0f)));
+	obj_container.addObject(new Cube(&mainShader, glm::vec3(5.0f, 0.0f, 0.0f)));
+	obj_container.addObject(new Cube(&mainShader, glm::vec3(-5.0f, 0.0f, 0.0f)));
 
 	projection_matrix = glm::perspective(glm::radians(45.0f), (GLfloat)screen_width / (GLfloat)screen_height, 0.1f, 100.0f);
 
+	// TEST
+	float init_time = static_cast<float>(glfwGetTime());
+	srand(static_cast<unsigned int>(time(0)));
+	int min = -10, max = 10;
+	double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -79,8 +88,17 @@ int main()
 
 		// Draw and manipulate stuff here
 
+		// TEST STUFF!
+		if (static_cast<float>(glfwGetTime()) - init_time >= 5.0f)
+		{
+			GLfloat x = static_cast<float>(min + (max - min + 1) * rand() * fraction);
+			GLfloat y = static_cast<float>(min + (max - min + 1) * rand() * fraction);
+			obj_container.addObject(new Cube(&mainShader, glm::vec3(x, y, 0.0f)));
+			init_time = glfwGetTime();
+		}
+		
 		obj_container.updateAll();
-		//obj_container.drawAll();
+
 		// End draw and manipulate stuff here
 
 		glfwSwapBuffers(window);
